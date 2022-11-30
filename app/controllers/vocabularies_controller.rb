7acff4f16bd3851d48def357ea4b0dc4de3b1b0a
Daiki Iwata <direction2423@gmail.com>
2022-11-30 20:00:12 +0900

    同期通信でデータを表示

--- app/controllers/vocabularies_controller.rb
+++ app/controllers/vocabularies_controller.rb
@@ -1,6 +1,6 @@
 class VocabulariesController < ApplicationController
   def index
-    # @vocabularies = Vocabulary.order("id DESC")
-    # @vocabulary = Vocabulary.new
+    @vocabularies = Vocabulary.order("id DESC")
+    @vocabulary = Vocabulary.new
   end
 end
